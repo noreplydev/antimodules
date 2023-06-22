22da95efe964d64b9a5e6cdc0f1c4b9fce8a831d
noreplydev <contact.cristiansanchez@gmail.com>
2023-06-22 22:16:01 +0200

    [FT] It removes node_modules

--- src/main.rs
+++ src/main.rs
@@ -43,11 +43,12 @@ fn main() {
 
     println!("\n Files {}, Folders {},", results.files, results.folders);
     println!(" Node Modules {}", results.node_modules);
-    println!("\n Total {}\n", results.total);
+    println!("\n Total {}\n", results.);
 }
 
 fn traverse(dir: Option<&str>, analytics: &mut Analytics) -> Analytics {
     if dir == None {
+        println!("Last directory passed");
         return Analytics::new(
             analytics.files,
             analytics.folders,
@@ -74,30 +75,35 @@ fn traverse(dir: Option<&str>, analytics: &mut Analytics) -> Analytics {
         let path = file.path();
         let path = path.to_str().unwrap();
 
-        if file.file_type().unwrap().is_dir() {
-            let file_name = file.file_name();
-            let file_name = file_name.to_str().unwrap();
+        // In case of a file
+        if !file.file_type().unwrap().is_dir() {
+            println!("\r{}", path);
+            Analytics::add_file(analytics);
+        }
 
-            if file_name == "node_modules" {
-                Analytics::add_node_modules(analytics);
-                let remove_status = fs::remove_dir_all(path);
+        Analytics::add_folder(analytics);
 
-                match remove_status {
-                    Ok(_) => {
-                        println!("Removed {}", path);
-                    }
-                    Err(e) => {
-                        println!("Error removing {}: {}", path, e);
-                    }
+        let file_name = file.file_name();
+        let file_name = file_name.to_str().unwrap();
+
+        if file_name == "node_modules" {
+            Analytics::add_node_modules(analytics);
+            let remove_status = fs::remove_dir_all(path);
+
+            match remove_status {
+                Ok(_) => {
+                    println!("Removed {}", path);
                 }
-
-                continue;
+                Err(e) => {
+                    println!("Error removing {}: {}", path, e);
+                }
             }
 
+            continue;
+        }
+
+        if file.file_type().unwrap().is_dir() {
             traverse(Some(path), analytics);
-        } else {
-            println!("\r{}", path);
-            std::io::stdout().flush();
         }
     }
 
